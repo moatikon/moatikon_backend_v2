703253a22467372b85b36058525a33636ba7354d
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-26 16:44:43 +0900

    ✨ (user.entity.ts): 사용자 엔티티 클래스 추가 💡 사용자 정보를 관리하기 위한 User 엔티티를 생성하여 이메일, 비밀번호, 이름 속성을 정의함.

---
+++ src/user/entity/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryColumn()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+}

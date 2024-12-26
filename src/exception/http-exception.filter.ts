1e54b13e6c998defaccb3b794197a38d3ae3c105
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-26 17:19:49 +0900

    ✨ (http-exception): HTTP 예외 처리기 및 사용자 정의 예외 클래스 추가

---
+++ src/exception/http-exception.filter.ts
@@ -0,0 +1,21 @@
+import { Request } from 'express';
+import { HttpException } from './http.exception';
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const req: Request = ctx.getRequest();
+    const res = ctx.getResponse();
+
+    const status = exception.getStatus();
+
+    res.status(status).json({
+      statusCode: status,
+      message: exception.message,
+      path: req.path,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}

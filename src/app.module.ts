41f3102a0f6e1dc049e446ef295966daa6477070
ljyo2o9 <yggosxoljy@gmail.com>
2024-12-26 16:44:26 +0900

    ✨ (app.module.ts): 데이터베이스 설정을 위한 ConfigModule 추가 ✨ (app.module.ts): TypeOrmModule을 비동기적으로 설정하여 DB 연결 💡 (app.module.ts): 환경 변수 유효성 검사를 위한 Joi 스키마 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}

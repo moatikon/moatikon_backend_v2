c8a91934051fe67fb13b0e91bcaccce864ad906e
ljyo2o9 <yggosxoljy@gmail.com>
2025-01-02 15:05:55 +0900

    ✨ (auth): 구글 인증을 위한 Auth 모듈 및 서비스 추가 - AuthController, AuthService, AuthModule을 새로 생성하여   구글 인증 기능을 구현함. - 구글 사용자 정보를 처리하고 JWT를 생성하는 로직을 포함함.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,50 @@
+import { Repository } from 'typeorm';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenResponse } from './response/token.response';
+import { GoogleUser } from 'src/common/interface/google-user.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async generateJwt(googleUser: User, isRefreshToken: boolean) {
+    return await this.jwtService.signAsync(
+      {
+        email: googleUser.email,
+        isRefreshToken: isRefreshToken,
+      },
+      {
+        expiresIn: '24h',
+        secret: this.configService.get('JWT_SECRET'),
+      },
+    );
+  }
+
+  async googleCallback(googleUser: GoogleUser) {
+    const userData = await this.userRepository.findOne({
+      where: { email: googleUser.email },
+    });
+
+    if (userData) {
+      return new TokenResponse(
+        await this.generateJwt(userData, false),
+        await this.generateJwt(userData, true),
+      );
+    } else {
+      const user = this.userRepository.create({
+        email: googleUser.email,
+        name: googleUser.name,
+      });
+      await this.userRepository.save(user);
+    }
+  }
+}

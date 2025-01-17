b14fa171d5f54cdc4412f0fc5e5727e9488cd3fa
ljyo2o9 <yggosxoljy@gmail.com>
2025-01-17 15:32:00 +0900

    ✨ (auth.service.ts): 사용자 철회 기능 추가 및 데이터 소스 의존성 주입 ♻️ (auth.service.ts): 사용자 생성 로직 개선 및 예외 처리 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/entity/user.entity';
@@ -6,6 +6,8 @@ import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { TokenResponse } from './response/token.response';
 import { GoogleUser } from 'src/common/interface/google-user.interface';
+import { JwtPayload } from 'src/common/interface/jwt-payload';
+import { UserNotFoundException } from 'src/exception/error/user-not-found.exception';
 
 @Injectable()
 export class AuthService {
@@ -14,6 +16,7 @@ export class AuthService {
     private readonly userRepository: Repository<User>,
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    private readonly datasource: DataSource,
   ) {}
 
   async generateJwt(googleUser: GoogleUser, isRefreshToken: boolean) {
@@ -35,16 +38,49 @@ export class AuthService {
     });
 
     if (!userData) {
-      const user = this.userRepository.create({
+      await this.userRepository.save({
         email: googleUser.email,
         name: googleUser.name,
       });
-      await this.userRepository.save(user);
-    } 
+    } else if (!userData.available) {
+      userData.available = true;
+      userData.withdrawDate = null;
+      await this.userRepository.save(userData);
+    }
 
     return new TokenResponse(
       await this.generateJwt(googleUser, false),
       await this.generateJwt(googleUser, true),
     );
+  }
+
+  async withdraw(jwtPayload: JwtPayload) {
+    const qr = this.datasource.createQueryRunner();
+    await qr.connect();
+    await qr.startTransaction();
+
+    try {
+      const { email } = jwtPayload;
+      const userData = await this.userRepository.findOne({
+        where: { email },
+      });
+
+      if (!userData) {
+        throw new UserNotFoundException();
+      }
+
+      userData.available = false;
+      userData.withdrawDate = new Date();
+
+      await this.userRepository.save(userData);
+      await qr.commitTransaction();
+
+      return true;
+    } catch (err) {
+      await qr.rollbackTransaction();
+      throw err;
+    } finally {
+      await qr.release();
+    }
   }
 }

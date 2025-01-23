e5493d4cc695568db862764fdc3df47e7c931d46
ljyo2o9 <yggosxoljy@gmail.com>
2025-01-23 17:32:00 +0900

    ✨ (find-tikon.dto.ts): FindTikonDto에 available 필드 추가 💡 available 필드는 0 또는 1의 값을 가질 수 있도록 유효성 검사 추가

--- src/tikon/dto/find-tikon.dto.ts
+++ src/tikon/dto/find-tikon.dto.ts
@@ -1,8 +1,15 @@
-import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsIn, IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 export class FindTikonDto {
   @IsNumber()
   @IsOptional()
   @IsNotEmpty()
   page?: number;
+
+  @IsNumber()
+  @IsIn([0, 1])
+  @IsNotEmpty()
+  @IsOptional()
+  available?: number;
 }

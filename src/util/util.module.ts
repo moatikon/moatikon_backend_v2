de887753599ce412f5ff07df6a904202cb5bc674
ljyo2o9 <yggosxoljy@gmail.com>
2025-02-25 18:56:22 +0900

    ✨ (util.module.ts): 메일 서비스 기능 추가 및 MailerModule 설정 💡 MailerModule을 비동기적으로 설정하여 환경 변수 사용

--- src/util/util.module.ts
+++ src/util/util.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { S3Service } from './service/s3.service';
 import { FcmService } from './service/fcm.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { ConfigService } from '@nestjs/config';
+import { MailService } from './service/mail/mail.service';
 
 @Module({
-  imports: [],
-  providers: [S3Service, FcmService],
-  exports: [S3Service, FcmService],
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          host: 'smtp.gmail.com',
+          port: 587,
+          auth: {
+            user: config.get<string>('MAILER_USER'),
+            pass: config.get<string>('MAILER_PW'),
+          },
+        },
+        defaults: {
+          from: '"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: 'src/util/service/mail/templates/',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
+  ],
+  providers: [S3Service, FcmService, MailService],
+  exports: [S3Service, FcmService, MailService],
 })
 export class UtilModule {}
